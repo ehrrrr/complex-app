e9ba28a00c35808ef14da43566a2a91edf595b3b
Elisaveta Hristova <elisaveta.s.hristova@gmail.com>
2019-11-14 20:21:50 +0200

    Hashing password

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
-const usersConnection = require('../db').collection('users');
+const bcrypt = require('bcryptjs');
+const usersConnection = require('../db').db().collection('users');
 const validator = require('validator');
 
 const User = function (data) {
@@ -24,8 +25,8 @@ User.prototype.validate = function() {
     if(this.data.username !== "" && !validator.isAlphanumeric(this.data.username)) {this.errors.push("Username can only contain letters and numbers.")}
     if(!validator.isEmail(this.data.email)) { this.errors.push("You must provide a valid email") }
     if(this.data.password === "") { this.errors.push("You must provide a password") }
-    if(this.data.password.length > 0 && this.data.password.length < 12) { this.errors.push("Password must be at least 12 characters.") }
-    if(this.data.password.length > 100) { this.errors.push("Password cannot exceed 100 characters.") }
+    if(this.data.password.length > 0 && this.data.password.length < 8) { this.errors.push("Password must be at least 8 characters.") }
+    if(this.data.password.length > 50) { this.errors.push("Password cannot exceed 50 characters.") }
     if(this.data.username.length > 0 && this.data.username.length < 3) { this.errors.push("Username must be at least 3 characters.") }
     if(this.data.username.length > 30) { this.errors.push("Username cannot exceed 30 characters.") }
     
@@ -39,6 +40,9 @@ User.prototype.register = function() {
     // Step #2: Only if there are no validation error
     // then save the user into a database
     if(!this.errors.length) {
+        //hash user password
+        let salt = bcrypt.genSaltSync(10);
+        this.data.password = bcrypt.hashSync(this.data.password, salt);
         usersConnection.insertOne(this.data);
     }
 };
@@ -47,7 +51,7 @@ User.prototype.login = function() {
     return new Promise((resolve, reject) => {
         this.cleanUp();
         usersConnection.findOne({username: this.data.username}).then((attemptedUser)=>{
-            if(attemptedUser && attemptedUser.password === this.data.password) {
+            if(attemptedUser && bcrypt.compareSync(this.data.password, attemptedUser.password)) {
                 resolve('Congrats');
             } else {
                 reject('Invalid username and/or password');
