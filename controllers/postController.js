04e4b61eee292ad2338d7988becd9a5b97595e4a
Elisaveta Hristova <elisaveta.s.hristova@gmail.com>
2019-11-16 12:52:56 +0200

    Adding dynamic data in post view

--- controllers/postController.js
+++ controllers/postController.js
@@ -20,6 +20,6 @@ exports.viewSingle = async function(req, res) {
         let post = await Post.findSingleById(req.params.id);
         res.render('single-post-screen', {post: post});
     } catch {
-        res.send('404 Idiot');
+        res.status(404).render('404');
     }
 }
